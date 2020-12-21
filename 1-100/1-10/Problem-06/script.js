fb1bcc4e03ef89b2cfd2b3799a2796f93ffcab25
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2020-12-21 20:19:39 +0100

    Added p6

--- 1-100/1-10/Problem-06/script.js
+++ 1-100/1-10/Problem-06/script.js
@@ -1,28 +1,30 @@
 /*
-* Problem 6
-* The sum of the squares of the first ten natural numbers is
-*       1² + 2² + ... + 10² = 385
-*
-* The square of the sum of the first ten natural numbers is
-*       (1 + 2 + ... + 10)² = 55² = 3025
-*
-* Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
-*       3025 - 385 = 2640
-*
-* Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-*/
+ * Problem 6
+ * The sum of the squares of the first ten natural numbers is
+ *       1² + 2² + ... + 10² = 385
+ *
+ * The square of the sum of the first ten natural numbers is
+ *       (1 + 2 + ... + 10)² = 55² = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+ *       3025 - 385 = 2640
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
 
-const TARGET = 100;
-let sum_of_squares = 0, squares_of_sum = 0, difference;
-let i;
+let answer;
+let sum_of_squares = 0;
+let square_of_sums;
 
-for(i = 1; i <= TARGET; i++) {
+for (let i = 1; i <= 100; i++) {
     sum_of_squares += i * i;
 }
 
-// (n * (n+1)) / 2 = 1 + 2 + ... + (n - 1) + n
-squares_of_sum = (TARGET * (TARGET + 1)) / 2;
-squares_of_sum *= squares_of_sum;
+// (n * (n+1)) / 2 --> sum(1 -> n) = 1 + 2 + ... + (n - 1) + n
+square_of_sums = (100 * (100 + 1)) / 2;
+square_of_sums *= square_of_sums;
 
-difference = squares_of_sum - sum_of_squares;
-console.log(difference);
+answer = square_of_sums - sum_of_squares;
+
+// Answer : 25164150
+document.getElementById('answer').textContent = answer;
